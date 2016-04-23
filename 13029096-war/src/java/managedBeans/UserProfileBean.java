0b885803768d7a563e1f201ed4dfe8c6ede17a68
Abdul Halim <casper@MacBook-Pro-6.local>
2016-04-23 21:27:32 +0100

    Added viewing user profile functionality

--- 13029096-war/src/java/managedBeans/UserProfileBean.java
+++ 13029096-war/src/java/managedBeans/UserProfileBean.java
@@ -7,7 +7,10 @@ package managedBeans;
 
 import entity.Customer;
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import session.CustomerFacade;
 import session.Gp14userFacade;
 
@@ -15,6 +18,8 @@ import session.Gp14userFacade;
  *
  * @author Abdul Halim <13029096@studentmail.ul.ie>
  */
+@Named(value = "userProfileBean")
+@RequestScoped
 public class UserProfileBean {
     
     @EJB
@@ -23,6 +28,9 @@ public class UserProfileBean {
     @EJB
     private CustomerFacade cf;
     
+    @ManagedProperty(value = "#{param.custId}")
+    private Integer custId;
+
     /**
      * Creates a new instance of ViewProfileBean
      */
@@ -31,15 +39,42 @@ public class UserProfileBean {
     
     public Customer getCutomerDetails()
     {
-        String userName  = FacesContext.getCurrentInstance()
-                .getExternalContext().getRemoteUser();
-        if(userName != null)
+        Integer profileId = null;
+        System.err.println("Customer ID param: " + custId);
+        if(custId == null) // view request without parameters -> Viewing own profile
         {
-            Integer custId = uf.getCustomerIdByUsername(userName);
-            Customer customer = cf.getCustomerById(custId);
+            System.err.println("Customer ID is NULL: ");
+            String userName  = FacesContext.getCurrentInstance()
+                    .getExternalContext().getRemoteUser();
+            if(userName != null)
+            {
+                profileId = uf.getCustomerIdByUsername(userName);
+                System.err.println("Getting logged in users Id ");
+            }
+        }
+        else
+            profileId = custId;
+        
+        if(profileId != null)
+        {
+            Customer customer = cf.getCustomerById(profileId);
             return customer;
         }
-        return null;
+        else
+            return null;
+    }
+    
+    public boolean isOwnProfile()
+    {
+       return (custId == null);
+    }
+    
+        public Integer getCustId() {
+        return custId;
+    }
+
+    public void setCustId(Integer custId) {
+        this.custId = custId;
     }
     
 }
