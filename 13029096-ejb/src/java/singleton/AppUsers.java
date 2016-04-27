62866a8afae5b20f8ba6412e9c5ece6a952f19f6
Abdul Halim <casper@MacBook-Pro-6.local>
2016-04-27 21:38:29 +0100

    Creating more users for all customers

--- 13029096-ejb/src/java/singleton/AppUsers.java
+++ 13029096-ejb/src/java/singleton/AppUsers.java
@@ -5,6 +5,7 @@
  */
 package singleton;
 
+import java.util.*;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -19,7 +20,7 @@ import usersngroups.GroupEjb;
  *
  * This is a singleton bean that adds required users and groups in DB if not
  * exist at start up
- * 
+ *
  * @author Abdul Halim <13029096@studentmail.ul.ie>
  */
 @Singleton
@@ -29,45 +30,73 @@ public class AppUsers {
 
     @PersistenceContext
     private EntityManager em;
-    
+
     @EJB
     private CreateUser creatUser;
-    
+
     @EJB
     private GroupEjb gejb;
-    
-    
+
     @PostConstruct
     public void init() {
         // here you can perform queries or transactions
-        
+
         // Create user "joe" and add to groups
-        if(!creatUser.isUserExist("joe"))
-        {
+        if (!creatUser.isUserExist("joe")) {
             creatUser.createUser(1, "joe", "1D10T?");
+
+            if (!gejb.isUserInGroup(1, "user")) {
+                gejb.addUserToGroup(1, "joe", "user");
+            }
+            if (!gejb.isUserInGroup(1, "customer")) {
+                gejb.addUserToGroup(1, "joe", "customer");
+            }
         }
-        if(!gejb.isUserInGroup(1, "user"))
-        {
-            gejb.addUserToGroup(1, "joe", "user");
-        }
-        if(!gejb.isUserInGroup(1, "customer"))
-        {
-            gejb.addUserToGroup(1, "joe", "customer");
-        }
-        
-        
-        // Create user "toor" and add to groups 
-        if(!creatUser.isUserExist("toor"))
-        {
+
+        // Create Admin user
+        if (!creatUser.isUserExist("toor")) {
             creatUser.createUser(2, "toor", "4uIdo0!");
+
+            if (!gejb.isUserInGroup(2, "user")) {
+                gejb.addUserToGroup(2, "toor", "user");
+            }
+            if (!gejb.isUserInGroup(2, "admin")) {
+                gejb.addUserToGroup(2, "toor", "admin");
+            }
         }
-        if(!gejb.isUserInGroup(2, "user"))
+
+        // Create other users
+        Map<Integer, String> userList = new HashMap<>();
+
+        userList.put(25, "wren");
+        userList.put(3, "bill");
+        userList.put(36, "bob");
+        userList.put(106, "early");
+        userList.put(149, "john");
+        userList.put(863, "big");
+        userList.put(777, "west");
+        userList.put(753, "zed");
+        userList.put(722, "big");
+        userList.put(409, "media");
+        userList.put(410, "yankee");
+
+        for (Map.Entry<Integer, String> entry : userList.entrySet()) 
         {
-            gejb.addUserToGroup(2, "toor", "user");
+            
+            Integer custId = entry.getKey();
+            String userName = entry.getValue();
+            
+            if (!creatUser.isUserExist(userName)) {
+                creatUser.createUser(custId, userName, userName);
+
+                if (!gejb.isUserInGroup(custId, userName)) {
+                    gejb.addUserToGroup(custId, userName, "user");
+                }
+                if (!gejb.isUserInGroup(custId, "customer")) {
+                    gejb.addUserToGroup(custId, userName, "customer");
+                }
+            }
         }
-        if(!gejb.isUserInGroup(2, "admin"))
-        {
-            gejb.addUserToGroup(2, "toor", "admin");
-        }
+
     }
 }
