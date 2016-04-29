de343c39bb2937ca4e976605cc2156302faa7db1
Abdul Halim <casper@MacBook-Pro-6.local>
2016-04-29 18:32:45 +0100

    Adding missing MessageHandler.java file

---
+++ 13029096-ejb/src/java/session/MessageHandler.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Anon
+ */
+@Stateless
+public class MessageHandler implements MessageHandlerLocal {
+
+/**
+ *
+ * @author Benjamin
+    /**
+     * This class is intended to act as error message coordinator
+     * we can use it across the app  and dynamically decide what error messages to
+     * display at any given time
+     */
+    private String errorMessage;
+    private String message;
+/**
+ * 
+ * @return a errorMessage 
+ */
+    @Override
+    public String getErroMessage() {
+        return this.errorMessage;
+    }
+/**
+ * 
+ * @param message sets a errorMessage 
+ */
+    @Override
+    public void setErroMessage(String message) {
+        this.errorMessage = message;
+    }
+/**
+ * 
+ * @return a user Message
+ */
+    @Override
+    public String getMessage() {
+       return this.message;
+    }
+/**
+ * 
+ * @param message set a userMessage 
+ */
+    @Override
+    public void setMessage(String message) {
+       this.message = message;
+    }
+    
+}
